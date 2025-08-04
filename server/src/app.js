24be1b1f75ef0643f33784e795fd508cd810d403
dipanshu-official <hellodipanshu@47.com>
2025-08-04 17:56:58 +0530

    push

---
+++ server/src/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+
+export default app;
